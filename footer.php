acf77639be823d840aa3fec527223052f3147725
Pierre VERT <vert@obs-mip.fr>
2018-12-26 10:32:20 +0100

    update link footer

--- footer.php
+++ footer.php
@@ -24,14 +24,14 @@
             <h1>Sites outils</h1>
             <ul>
                 <li>
-                    <a href="http://pedagogie.ac-toulouse.fr/svt/serveur/sig/index.htm" title="Accéder aux ressources par thèmes d'activités et par outils logiciels"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/logoSIGs.jpg" alt="">SIGthèque</a>
+                    <a href="https://disciplines.ac-toulouse.fr/svt/des-sig-dans-l-enseignement-des-svt" title="Accéder aux ressources par thèmes d'activités et par outils logiciels"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/logoSIGs.jpg" alt="">SIGthèque</a>
                 </li>
                 <li><a href="http://planet-terre.ens-lyon.fr/" title="Planet Terre ENS de Lyon"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/logo-pt2.png" alt="">Planet Terre</a></li>
                 <li><a href="http://infoterre.brgm.fr/"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/logo-it.png" alt="">Info Terre</a></li>
                 <li><a href="http://www.geoportail.gouv.fr/accueil" title="Interface cartographique GéoPortail"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/logo-gp.png" alt="">Géo Portail</a></li>
                 <li><a href="http://www.edusismo.org/" title="Sismos à l'école"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/logo-sism.jpg" alt="">Sismo Ecoles</a></li>
                 <li><a href="http://www.brgm.fr/decouverte/jeunes-education/edutheque-ressources-enseignement-geosciences" title="Eduthèque : des ressources pour l’enseignement des géosciences"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/logo-et.jpg" alt="">Edu Terre</a></li>
-                <li><a href="http://eduscol.education.fr/svt/enseigner/ressources-et-usages-numeriques.html" title="Portail national de ressources éduscol, Sciences de la Vie et de la Terre"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/educ-nat1.gif" alt="">Lithothèques</a></li>
+                <li><a href="https://lithotheque.ac-montpellier.fr/geoduc" title="Portail national de ressources éduscol, Sciences de la Vie et de la Terre"><img width="50px" src="<?php echo get_bloginfo('template_directory') ?>/images/educ-nat1.gif" alt="">Lithothèques</a></li>
             </ul>
         </section>
         <section></section>
